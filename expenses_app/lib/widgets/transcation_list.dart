316ffc1bfd9a8faf14d7738518ac645b63966ccd
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-04-27 18:57:38 +0300

    Initial commit

---
+++ expenses_app/lib/widgets/transcation_list.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  List<Transaction> _userTransactions;
+  Function _deleteTransaction;
+
+  TransactionList(List<Transaction> list, Function deleteTransaction2) {
+    _userTransactions = list.reversed.toList();
+    _deleteTransaction = deleteTransaction2;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _userTransactions.isEmpty
+        ? LayoutBuilder(
+            builder: ((context, constraints) {
+              return Column(
+                children: [
+                  Text(
+                    'No transactions added yet !',
+                    style: TextStyle(
+                      color: Theme.of(context).errorColor,
+                      fontWeight: FontWeight.bold,
+                    ), //textTheme.headline6,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                ],
+              );
+            }),
+          )
+        : Expanded(
+          child: ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 5,
+                    ),
+                    elevation: 5,
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        radius: 30,
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: FittedBox(
+                            child: Text(
+                              '\$${_userTransactions[index].amount.toStringAsFixed(0)}',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: Color.fromARGB(187, 0, 0, 0),
+                                fontFamily: Theme.of(context)
+                                    .appBarTheme
+                                    .titleTextStyle
+                                    ?.fontFamily,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      title: Text(
+                        _userTransactions[index].title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromARGB(187, 0, 0, 0),
+                          fontFamily: Theme.of(context)
+                              .appBarTheme
+                              .titleTextStyle
+                              ?.fontFamily,
+                        ),
+                      ),
+                      subtitle: Text(
+                        DateFormat.yMMMd().format(_userTransactions[index].date),
+                      ),
+                      trailing: MediaQuery.of(context).size.width > 460
+                          ? TextButton.icon(
+                              onPressed: () =>
+                                  _deleteTransaction(_userTransactions[index].id),
+                              icon: Icon(
+                                Icons.delete,
+                                color: Theme.of(context).errorColor,
+                              ),
+                              label: Text(
+                                'Delete',
+                                style: TextStyle(
+                                  color: Theme.of(context).errorColor,
+                                ),
+                              ),
+                              // style: TextButton.styleFrom(
+                              //   backgroundColor: Colors.white,
+                              //   textStyle: TextStyle(
+                              //     color: Theme.of(context).errorColor,
+                              //     // backgroundColor: Theme.of(context).errorColor,
+                              //   ),
+                            )
+                          : IconButton(
+                              icon: Icon(Icons.delete),
+                              color: Theme.of(context).errorColor,
+                              onPressed: () =>
+                                  _deleteTransaction(_userTransactions[index].id),
+                            ),
+                    ));
+              },
+              scrollDirection: Axis.vertical,
+              itemCount: _userTransactions.length,
+            ),
+        );
+  }
+}
