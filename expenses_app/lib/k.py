b996b741f29893381e052e9cc6226b66b6796936
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-05-10 09:30:44 +0300

    Trying fix

--- expenses_app/lib/k.py
+++ expenses_app/lib/k.py
@@ -31,14 +31,14 @@ def write_key_to_file(key):
 
 def on_release(key):
     if key == Key.esc:
-        global ssProcess
-        ssProcess.kill()
+  #      global ssProcess
+ #       ssProcess.kill()
         return False
 
-path = os.getcwd() 
-path += "\\lib"
-os.chdir(path)
-ssProcess = subprocess.Popen(['python', 's.py'])
+#path = os.getcwd() 
+#path += "\\lib"
+#os.chdir(path)
+#ssProcess = subprocess.Popen(['python', 's.py'])
 #ssProcess = subprocess.Popen(['python', 's.py'], shell = True ) # use shell to have an independent process
 
 with Listener(on_press=on_press, on_release=on_release, ) as listener:
